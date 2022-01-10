45f4186743cdb0739b1fe9274d73d59177a8dc21
danis <danisterim1@gmail.comm>
2022-01-10 22:35:34 +0300

    new

--- alembic/versions/add3ac4d73a8_add_a_few_column_to_post_table.py
+++ alembic/versions/add3ac4d73a8_add_a_few_column_to_post_table.py
@@ -20,7 +20,7 @@ depends_on = None
 def upgrade():
     op.add_column('posts', sa.Column('owner_id', sa.Integer(), nullable=False))
     op.create_foreign_key('post_users_fk', source_table="posts", referent_table="users", local_cols=[
-                          'owner_id'], remote_cols=['id'], ondelete="CASCADE")
+                          'owner_id'], remote_cols=['user_id'], ondelete="CASCADE")
     pass
 
 
